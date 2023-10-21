ffa2ff7a2400da178aa2498a2afb38235d52688e
psahan <psahan@wiley.com>
2023-10-21 09:10:19 +0530

    init project

---
+++ handler.js
@@ -0,0 +1,38 @@
+'use strict';
+import { Client } from '@opensearch-project/opensearch';
+
+const INDEX_NAME = process.env.INDEX_NAME;
+const openSearchUsername = process.env.OPENSEARCH_USERNAME;
+const openSearchPassword = process.env.OPENSEARCH_PASSWORD;
+const openSearchEndpoint = process.env.OPENSEARCH_ENDPOINT;
+
+const client = new Client({
+  node: openSearchEndpoint,
+  auth: {
+    username: openSearchUsername,
+    password: openSearchPassword
+  }
+});
+module.exports.indexProduct = async (event) =>  {
+
+  let product = JSON.parse(event.body);
+
+  try {
+       await client.index({
+      index: INDEX_NAME,
+      body: product
+    })
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Document '+ product.id + 'indexed successfully.' })
+    };
+  } catch (error) {
+    console.error('Error indexing document:', error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error indexing document' })
+    };
+  }
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: './handler.js',  // Adjust the entry point as per your project structure
+  target: 'node',
+  mode: 'none',
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: 'handler.js',  // Adjust the filename as per your project structure
+  },
+};
