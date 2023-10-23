f82a656e9a7e442f8ed0444db820c36001c1354f
psahan <psahan@wiley.com>
2023-10-23 07:24:15 +0530

    added 3rd party integration

--- products.js
+++ products.js
@@ -1,8 +1,46 @@
-module.exports.getProduct = async (event) =>  {
+const axios = require('axios');
+const opensearch = require('./indexCreation')
 
-  const eventObj = event.body;
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: JSON.parse(eventObj) })
-  };
+module.exports.getProduct = async (event) => {
+  const baseUrl = process.env.PRODUCT_URL;
+  const AUTH_TOKEN = process.env.X_WPP_AUTH_TOKEN
+  const token = event.headers[AUTH_TOKEN]
+
+  let requestBody;
+  try {
+    requestBody = JSON.parse(event.body);
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Invalid JSON in the request body' }),
+    };
+  }
+
+  const id = requestBody.id;
+
+  if (id == null || !id) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Missing "id" parameter in request body' }),
+    };
+  }
+
+  const headers = {
+    'X-WPP-AUTH-TOKEN': token,
+  }
+
+  const url = `${baseUrl}/${id}`
+  try {
+    const response = await axios.get(url, { headers })
+    opensearch.indexCreation(response.data);
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify(response.data),
+    // };
+  } catch (error) {
+    return {
+      statusCode: error.response.status,
+      body: JSON.stringify(error.message),
+    };
+  }
 }
